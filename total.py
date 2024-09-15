f714b3f4ab615a0a9c5dfabb1b65adb4c819c02d
Achmad Atha <145043698+athazayyan@users.noreply.github.com>
2024-09-15 19:10:57 +0700

    Update total.py

--- total.py
+++ total.py
@@ -95,8 +95,8 @@ for year in range(df.index.max() + 1, future_years + 1):
     future_X.index = [year]
     pred = model.predict(future_X)
     predictions.append((year, pred[0]))
-    future_X['gdp'] *= 1.0511  # Indonesia 5.11% annual GDP pertumbuhan
-    future_X['population'] *= 1.088  # 0.88% annual population growth
+    future_X['gdp'] *= 1.05  # Indonesia 5.11% annual GDP pertumbuhan
+    future_X['population'] *= 1.08  # 0.88% annual population growth
     # Add assumptions for other features if necessary
 
 predictions_df = pd.DataFrame(predictions, columns=['year', 'predicted_co2']).set_index('year')
